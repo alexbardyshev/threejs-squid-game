88c8f208a51617c882fb41fa8440a3ca3eb897bf
Oleksìi <bardyshev@gmail.com>
2022-09-22 08:52:39 +0300

    Doll was added to set with bluish background

--- js/main.js
+++ js/main.js
@@ -5,11 +5,35 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setClearColor(0xb7c3f3, 1);
 
-camera.position.z = 4;
+const light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
 
-renderer.render( scene, camera );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader();
+loader.load("../models/scene.gltf", function (gltf) {
+    scene.add(gltf.scene);
+    gltf.scene.scale.set(0.4, 0.4, 0.4);
+    gltf.scene.position.set(0, -1, 0);
+});
+
+function animate() {
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+}
+animate();
+
+window.addEventListener('resize', onWindowResize,  false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
